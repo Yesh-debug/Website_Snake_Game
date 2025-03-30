3ada544159d28d41689f95c7f3d8711f43d62e2c
Yesh-debug <yshkryslr@gmail.com>
2025-03-30 18:16:42 +0800

    Add files via upload

---
+++ login-system-main/connect/connection.php
@@ -0,0 +1,16 @@
+<?php 
+
+$dbServername = "localhost";
+$dbUsername = "yeshua";
+$dbPassword = "Krysler18";
+$dbName = "game_db";
+
+$connect = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
+
+/*
+// Check connection
+if ($mysqli -> connect_error) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}*/
+?>
